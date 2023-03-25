2ebc3265637420e1b308b4d3e90cf2a23c4d579e
fajar hidayat <fajarh247@gmail.com>
2023-03-25 13:17:58 +0700

    fix db

--- configs/DB.js
+++ configs/DB.js
@@ -1,13 +1,13 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const Conn = new Sequelize("backend", "root", "", {
-	host: "localhost",
-	port: "3306",
-	dialect: "mysql",
-});
-// const dbUri = process.env.DB_URL;
-// const Conn = new Sequelize(String(dbUri), {
+// const Conn = new Sequelize("backend", "root", "", {
+// 	host: "localhost",
+// 	port: "3306",
 // 	dialect: "mysql",
 // });
+const dbUri = process.env.DB_URL;
+const Conn = new Sequelize(String(dbUri), {
+	dialect: "mysql",
+});
 module.exports = Conn;
