e1bc64dc3b308b082f5c6a373629545b8efe73ec
fajar hidayat <fajarh247@gmail.com>
2023-03-25 12:33:18 +0700

    deploy heroku

--- configs/DB.js
+++ configs/DB.js
@@ -1,9 +1,11 @@
 const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
-const Conn = new Sequelize("backend", "root", "", {
-	host: "localhost",
-	port: "3306",
-	dialect: "mysql",
-});
+// const Conn = new Sequelize("backend", "root", "", {
+// 	host: "localhost",
+// 	port: "3306",
+// 	dialect: "mysql",
+// });
 
+const Conn = new Sequelize(process.env.DB_URL);
 module.exports = Conn;
